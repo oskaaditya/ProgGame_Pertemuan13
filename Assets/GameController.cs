42be120f2762540c34c008f9fd67c08a7744b88b
oskaaditya <oska.aditya18@gmail.com>
2020-12-06 18:39:06 +0700

    tugas pertemuan 13

---
+++ Assets/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    //Komponen GUI
+    public GameObject startPanel;
+    public int playerScore = 0;
+    public Text hitungText;
+    public Text hitungNyawa;
+    public int ronde = 1;
+    public GameObject rondeTeks;
+    public Text jmlRonde;
+    public Text teksJmlRonde;
+    public Text targetTeks;
+    public int tembakPerRonde = 3;
+    private int nyawa = 2;
+    public GameObject[] peluru;
+    
+    // show/hide GameObject
+    public GameObject GUITeksSkor;
+    public GameObject GUITeksNyawa;
+    public GameObject GUITarhetBidikan;
+    public GameObject GUITembak;
+    public GameObject GUIAnjing;
+    public GameObject GUITeksRonde;
+    public GameObject GUIGameOverPanel;
+    public GameObject GUIStartPanel;
+    public GameObject Terrain;
+    // control Audio
+    AudioSource audio;
+    public AudioClip[] clips;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
